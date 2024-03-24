e37a84f80f8e7e2299983f0a64ea3cf59750629a
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-03-24 07:49:02 -0400

    [TASK] Adds images and templates

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
+from projects import projects
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+  return render_template('projects.html', projects=projects)
+
+@app.route('/contact')
+def contact():
+  return render_template('contact.html')
 
 if __name__ == '__main__':
   app.run(debug = True, port = 3000, host='localhost')
---
+++ projects.py
@@ -0,0 +1,19 @@
+projects = [
+  {
+    "image": "/static/images/let-do-brunch-title-768x526.png",
+    "name": "Let&#8217;s Do Brunch!",
+    "techs": ["python", "flask", "css", "bootstrap", "html"],
+    "description": "This is my final project from the popular Harvard CS50 course taught by David Malan."
+    "live": "https://heidi37.pythonanywhere.com/",
+    "repo": "https://github.com/heidi37/cs50-final-project"
+  },
+    {
+    "image": "",
+    "name": "",
+    "techs": [],
+    "description": ""
+    "live": "",
+    "repo": ""
+  },
+
+]
