b1a8e7ea76e229e7d1fd9017a499faf48d3b1740
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-03-30 07:22:30 -0400

    adds project and improves tech stack styles

--- projects.py
+++ projects.py
@@ -2,8 +2,8 @@ projects = [
   {
     "image": "/static/images/let-do-brunch-title-768x526.png",
     "name": "Let's Do Brunch!",
-    "techs": ["python", "flask", "css", "bootstrap", "html"],
-    "description": "This is my final project from the popular Harvard CS50 course taught by David Malan.",
+    "techs": ["python", "flask", "css", "bootstrap", "html", "sql"],
+    "description": "This is my final project from the popular Harvard CS50 course taught by David Malan. It is a full stack app with sqlite database. The app allows users to plan a brunch and hopefully prevent too many people from bringing doughnuts.",
     "live": "https://heidi37.pythonanywhere.com/",
     "repo": "https://github.com/heidi37/cs50-final-project",
     "featured": True
@@ -12,8 +12,16 @@ projects = [
     "image": "/static/images/sticky-notes-768x526.png",
     "name": "Sticky Notes",
     "techs": ["react", "css", "html", "netlify"],
-    "description": "Eco-friendly React application that saves time and trees",
+    "description": "Eco-friendly React application that saves time and trees. The app utilizes local storage to save the notes even when the browser has been closed.",
     "live": "https://sticky-note-2024.netlify.app/",
     "repo": "https://github.com/heidi37/sticky-notes-2024"
+  },
+    {
+    "image": "/static/images/guess-the-word@2x-768x524.jpg",
+    "name": "Guess The Word",
+    "techs": ["javascript", "css", "html"],
+    "description": "This is an interactive game written with JavaScript. I used array iteration to compare the array string of the answer with the array string of the guesses. This was built with Vanilla JavaScript, no libraries.",
+    "live": "https://heidifryzell.com/guess-the-word/",
+    "repo": ""
   }
 ]
